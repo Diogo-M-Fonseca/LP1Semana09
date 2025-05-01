5fa9e86e16d00eeed249e1143443dc7af68ff075
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-01 21:12:57 +0100

    Player class changes and program exercise 1 finished

---
+++ PlayerManager1/Player.cs
@@ -0,0 +1,14 @@
+namespace PlayerManager1
+{
+    public class Player
+    {
+        private string Name {get;}
+        public int Score;
+        public Player(string Name, int Score)
+        {
+            this.Name = Name;
+            this.Score = Score;
+        }       
+
+    }
+}
