d6bdb07d078901d2203fd20f8eb7e69e03347d54
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-01 22:45:02 +0100

    exercise 6 setup

---
+++ PlayerManager4/Player.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace PlayerManager4
+{
+    public class Player : IComparable<Player>
+    {
+        public string Name {get;}
+        public int Score {get; set;}
+        
+        public Player(string Name, int Score)
+        {
+            this.Name = Name;
+            this.Score = Score;
+        }       
+        public int CompareTo(Player other)
+        {
+            if (other == null) return -1;
+
+            int result = other.Score.CompareTo(this.Score);
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return string.Compare(this.Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Score}";
+        }
+
+    }
+}
